c9964556ff6587e023cf5257fbaa9823f4d4ee14
Shara X Op <160161353+SharaXOp@users.noreply.github.com>
2024-04-12 10:02:03 +0530

    Create __init__.py

---
+++ SharaXmusic/__init__.py
@@ -0,0 +1,28 @@
+from SharaXmusic.core.bot import DAXX
+from SharaXmusic.core.dir import dirr
+from SharaXmusic.core.git import git
+from SharaXmusic.core.userbot import Userbot
+from SharaXmusic.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = DAXX()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
